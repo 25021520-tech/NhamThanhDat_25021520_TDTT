a2634939f829372b94274ff20c433560a48b49e6
25021516-tech <25021520@vnu.edu.vn>
2025-11-26 18:23:50 +0700

    kj

--- Th1.py
+++ Th1.py
@@ -1,2 +1,9 @@
-print ('0')
-a = 0
+# Name_Book = ['Book A', 'Book B', 'Book C']
+# Quantity = [1,2,3]
+# price = [100,200,300]
+# total = Quantity[0]*price[0]+Quantity[1]*price[1]
+# print("Giá trị đơn hàng của 3 cuốn sách đầu tiên là: ", total,"$")
+x=5
+print(x,type(x))
+x= 'Hello'
+print(x,type(x))
